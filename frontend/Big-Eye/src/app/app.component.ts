5245465f456dfbd9ae9beb7ff30668f1f9dc6df0
LinuxMint <linuxmint@linuxmint201.linuxvmimages.local>
2021-05-05 15:40:45 -0400

    Añado identificacion de usuarios

--- frontend/Big-Eye/src/app/app.component.ts
+++ frontend/Big-Eye/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Auth, Hub, Logger } from 'aws-amplify';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Big-Eye';
+
+  constructor() {
+    const listener = (data: any) => console.log(data);
+    Hub.listen('auth', listener);
+  }
+
+  private getJwtToken(): Promise<string | void> {
+    return Auth.currentSession()
+      .then(session => session.getIdToken().getJwtToken())
+      .catch(err => console.log(err));
+  }
+
+  public testAPICall(): void {
+    this.getJwtToken()
+      .then(console.log);
+  }
 }
